11ba91451ec5a3ab7dd578727c8351f51ab68af5
Steven Cardenas Oviedo <stevenco200@gmail.com>
2023-11-23 11:05:41 -0500

    Se agrego ele javascript para la funcion sumBinaryFactional

---
+++ js/interfaz.js
@@ -0,0 +1,39 @@
+const pantalla = document.querySelector('.pantalla');
+const botones = document.querySelectorAll('.btn');
+
+let numeroEnPantalla = '';
+let primerNumero = '';
+let segundoNumero = '';
+let operacion = '';
+
+botones.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    const valorBoton = boton.textContent.trim();
+
+    if (valorBoton === 'C') {
+      numeroEnPantalla = '0';
+      primerNumero = '';
+      segundoNumero = '';
+      operacion = '';
+    } else if (valorBoton === '←') {
+      numeroEnPantalla = numeroEnPantalla.slice(0, -1);
+    } else if (valorBoton === '+') {
+      primerNumero = numeroEnPantalla;
+      operacion = '+';
+      numeroEnPantalla = '';
+    } else if (valorBoton === '=') {
+      segundoNumero = numeroEnPantalla;
+      if (operacion === '+' && primerNumero && segundoNumero) {
+        const resultado = sumBinaryFractional(primerNumero, segundoNumero);
+        numeroEnPantalla = resultado;
+        primerNumero = resultado;
+        segundoNumero = '';
+        operacion = '';
+      }
+    } else {
+      numeroEnPantalla += valorBoton;
+    }
+
+    pantalla.textContent = numeroEnPantalla;
+  });
+});
---
+++ js/main.js
@@ -0,0 +1,41 @@
+function binaryToDecimal(binary) {
+    const parts = binary.split('.'); // Separar la parte entera de la fraccionaria
+    let integerPart = parseInt(parts[0], 2); // Convertir la parte entera a decimal
+
+    let fractionalPart = 0;
+    if (parts.length === 2) {
+        let fraction = parts[1];
+        for (let i = 0; i < fraction.length; i++) {
+            fractionalPart += parseInt(fraction[i], 2) / Math.pow(2, i + 1); // Convertir la parte fraccionaria a decimal
+        }
+    }
+
+    return integerPart + fractionalPart; // Sumar parte entera y fraccionaria
+}
+
+function decimalToBinary(decimal) {
+    const integerPart = Math.floor(decimal);
+    let binaryInteger = (integerPart >>> 0).toString(2); // Convertir la parte entera a binario
+
+    let fractionalPart = decimal - integerPart;
+    let binaryFractional = '';
+    if (fractionalPart !== 0) {
+        binaryFractional = '.';
+        while (fractionalPart !== 0) {
+            fractionalPart *= 2;
+            if (fractionalPart >= 1) {
+                binaryFractional += '1';
+                fractionalPart -= 1;
+            } else {
+                binaryFractional += '0';
+            }
+        }
+    }
+
+    return binaryFractional.length > 1 ? binaryInteger + binaryFractional : binaryInteger; // Retornar binario completo
+}
+
+function sumBinaryFractional(a, b) {
+    const decimalResult = binaryToDecimal(a) + binaryToDecimal(b);
+    return decimalToBinary(decimalResult);
+}
