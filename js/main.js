2d592d2731b5266b8c38738af0a4ce636045fd80
Steven Cardenas Oviedo <stevenco200@gmail.com>
2023-11-23 09:03:02 -0500

    se agrego el primer fracmento del javascript y se agrego el archivo interfaz.js para crear el js conterniente a la interaccion de la interfaz grafica de la calculadora

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-    
+function agregarNumero(numero) {
+    const pantalla = document.getElementById('pantalla');
+    if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+        pantalla.textContent = numero;
+    } else {
+        pantalla.textContent += numero;
+    }
+}
+
+function limpiarPantalla() {
+    document.getElementById('pantalla').textContent = "0";
+}
+
+function realizarSuma() {
+    const num1 = '10.1101'; // Ingresa el primer número binario
+    const num2 = '110.01'; // Ingresa el segundo número binario
+    const resultado = sumarBinariosConFraccion(num1, num2);
+    document.getElementById('pantalla').textContent = resultado;
+}
